b68ab9adc4841b62a8ab1447f0dee73ad73c4882
Tyler Frick <tyfrick25@gmail.com>
2020-08-14 15:31:01 -0700

    reshaped the badges

--- duel-links/src/containers/Homepage.js
+++ duel-links/src/containers/Homepage.js
@@ -9,21 +9,17 @@ class Homepage extends Component {
             <div className="Homepage">
                 <div>
                     <h1>Welcolme to the Duel Links Academy!</h1>
-                    <p>dummy content</p>
+                    <p>A fansite for the popular mobile game, no affiliation with konami</p>
                     <Logo />
                 </div>
-                <ul style={{display: "inline-grid", background: "no-repeat", alignContent: "center", padding: "0 20px"}}>
-                    <li>
-                        <a href='https://play.google.com/store/apps/details/?id=jp.konami.duellinks&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
-                            <img  style={{ maxWidth: "135px", height: "auto"}} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://apps.apple.com/de/app/yu-gi-oh-duel-links/id1068378177?mt=8">
-                            <img style={{maxWidth: "135px", maxHeight: "40px"}} alt="Get it from app store" src='https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2017-01-18&kind=iossoftware&bubble=ios_apps'/>
-                        </a>
-                    </li>
-                </ul>
+                <div style={{display: "inline-grid", background: "no-repeat", alignContent: "center", padding: "0 20px"}}>
+                    <a href='https://play.google.com/store/apps/details/?id=jp.konami.duellinks&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                        <img  style={{ width: "auto%", height: "55px"}} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+                    </a>
+                    <a href="https://apps.apple.com/de/app/yu-gi-oh-duel-links/id1068378177?mt=8">
+                        <img style={{ width: "88%", margin: "6%"}} alt="Get it from app store" src='https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2017-01-18&kind=iossoftware&bubble=ios_apps'/>
+                    </a>
+                </div>
                 <div>
                     <h1>Additional Duel Links Content Creators on Youtube:</h1>
                     <ul>
