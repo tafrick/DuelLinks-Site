b1d7df04fad6dec3afa286b9905a4f92a9aa7bee
Tyler Frick <tyfrick25@gmail.com>
2020-08-13 21:43:35 -0700

    added apple badge

--- duel-links/src/containers/Homepage.js
+++ duel-links/src/containers/Homepage.js
@@ -11,10 +11,19 @@ class Homepage extends Component {
                     <h1>Welcolme to the Duel Links Academy!</h1>
                     <p>dummy content</p>
                     <Logo />
-                    <a href='https://play.google.com/store/apps/details/?id=jp.konami.duellinks&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
-                        <img style={{maxHeight: "250px", maxWidth: "250px"}} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
-                    </a>
                 </div>
+                <ul style={{display: "inline-grid", background: "no-repeat", alignContent: "center", padding: "0 20px"}}>
+                    <li>
+                        <a href='https://play.google.com/store/apps/details/?id=jp.konami.duellinks&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                            <img  style={{ maxWidth: "135px", height: "auto"}} alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://apps.apple.com/de/app/yu-gi-oh-duel-links/id1068378177?mt=8">
+                            <img style={{maxWidth: "135px", maxHeight: "40px"}} alt="Get it from app store" src='https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2017-01-18&kind=iossoftware&bubble=ios_apps'/>
+                        </a>
+                    </li>
+                </ul>
                 <div>
                     <h1>Additional Duel Links Content Creators on Youtube:</h1>
                     <ul>
