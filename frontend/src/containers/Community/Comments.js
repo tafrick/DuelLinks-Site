a501cec5dd91df2f3708e33a7f42fca1c1d72363
Benny <bennyislife@gmail.com>
2020-09-24 23:05:48 -0700

    fixed time

--- frontend/src/containers/Community/Comments.js
+++ frontend/src/containers/Community/Comments.js
@@ -6,44 +6,42 @@ import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 import './Comments.css';
 
-const formatDate = (dateTime) => {
-    const time = dateTime;
-    console.log('dateTime' , dateTime);
-    dateTime = dateTime.substring(0, dateTime.indexOf('T'));
-    console.log('dateTime' , dateTime);
+const formatDateAndTime = (dateTime) => {
+    dateTime = new Date(dateTime);
+    
     const today = new Date();
-
     const currentYear = today.getFullYear();
     const currentMonth = '0'+ (today.getMonth()+1).toString().slice(-2);
     const currentDay = today.getDate().toString().slice(-2);
 
-    const getYear = dateTime.substring(0, dateTime.indexOf('-'));
-    const getMonth = dateTime.substring(dateTime.indexOf('-')+1, dateTime.indexOf('-')+3);
-    const getDay = dateTime.substring(dateTime.lastIndexOf('-')+1, dateTime.length);
+    const getYear = dateTime.getFullYear();
+    const getMonth = '0' + (dateTime.getMonth()+1).toString().slice(-2);
+    const getDay = dateTime.getDate().toString().slice(-2);
+    const getHour = dateTime.getHours();
+    const getMinute = ('0' + dateTime.getMinutes()).toString().slice(-2);
+    const timeStamp = getHour > 11 ? 
+    (getHour-12).toString() + ':' + getMinute + 'pm' : 
+    getHour + ':' + getMinute;
+
+    // console.log('getYear: ' , getYear);
+    // console.log('getMonth: ' , getMonth);
+    // console.log('getDay: ' , getDay);
+    // console.log('getHour: ' , getHour);
+    // console.log('getMinute: ' , getMinute);
     
     const yearDifference = (currentYear - getYear)*30;
     const monthDiffercence = (currentMonth - getMonth)*30;
     const dayDifference = Math.abs(currentDay - getDay);
+
+    // console.log('currentDay: ', currentDay);
+    // console.log('getDay: ', getDay);
+    // console.log('difference: ', dayDifference);
     
     const result = dayDifference === 0 ? 'Today' : (dayDifference > 1 ? dayDifference + ' days ago ' : ' Yesterday');
-console.log('result: ', result);
-    return result === 'Today' || result === 'Yesterday' ? result + ' ' + formatTime(time) : result;
+    // console.log('result: ', result);
+    return result === 'Today' || result === 'Yesterday' ? result + ' ' + timeStamp : result;
 }
 
-const formatTime = (dateTime) => {
-    let spam = (dateTime.substring(dateTime.lastIndexOf(':'), dateTime.length));
-    let timeOfDay = dateTime.substring(dateTime.lastIndexOf('T')+1, dateTime.lastIndexOf('T')+3);
-    console.log();
-    
-    timeOfDay = timeOfDay < 12 ? 'AM': 'PM';
-    dateTime = dateTime.replace(spam, timeOfDay).replace('T', ' ');
-    dateTime = dateTime.substring(dateTime.lastIndexOf(' ', dateTime.length));
-    dateTime = timeOfDay === 'PM' ? dateTime.replace(dateTime.substring(0, 3), dateTime.substring(0,3) - 12) : dateTime;
-    return dateTime;
-}
-
-// 2020-
-//
 const Comment = (props) => {
     return (
         <div className="Comment">
@@ -53,7 +51,7 @@ const Comment = (props) => {
                 <ArrowDownwardIcon className="downvote" onClick={props.clickedDown} />
             </div>
             <div className="Comment-title">
-                <span className="Comment-user">{formatDate(props.time)} </span>
+                <span className="Comment-user">{formatDateAndTime(props.time)} </span>
                 <span className="Comment-user">Posted by </span>
                 <span className="Comment-user underline">{props.username}</span>
                 
