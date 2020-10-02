c36f4ffbfaacac9318d83a5af4cf6d8c884cf21b
Tyler Frick <tyfrick25@gmail.com>
2020-10-02 15:44:23 -0700

    enforced the one like per user on comments

--- frontend/src/containers/Community/FullPost.js
+++ frontend/src/containers/Community/FullPost.js
@@ -17,7 +17,6 @@ class FullPost extends Component {
         this.state = {
             loadedPost: null,
             loadedcomments: [],
-            username: 'Weevil',
             newCommentBody: '',
             postID: '',
             displayPost: false,
@@ -27,9 +26,9 @@ class FullPost extends Component {
     }
 
     displayButton = () => {
-        this.setState({displayButton: !this.state.displayButton})
+        this.setState({ displayButton: !this.state.displayButton })
     }
-    toggleFullPost = () =>{
+    toggleFullPost = () => {
         this.setState({
             // this.toggleFullPost: !this.state.toggleFullPost
             toggleFullPost: !this.state.toggleFullPost
@@ -62,7 +61,7 @@ class FullPost extends Component {
 
     postDataHandler = () => {
         const newComment = {
-            username: this.state.username,
+            username: this.props.email,
             body: this.state.newCommentBody,
             postId: this.state.postID
         };
@@ -95,35 +94,130 @@ class FullPost extends Component {
         })
     }
 
-    upvoteHandler(commentID, oldUpvotes) {
-        const newUpvotes = oldUpvotes + 1;
-        const updatedComment = {
-            upvotes: newUpvotes
+    upvoteHandler(commentID, oldUpvotes, likeList, dislikeList) {
+        if (this.props.isAuth) {
+            let newUpvotes = 0;
+            let updatedComment = {};
+            let newLikedList = [];
+            //case 1
+            if (!dislikeList.includes(this.props.email) && !likeList.includes(this.props.email)) {
+                newUpvotes = oldUpvotes + 1;
+                newLikedList = [...likeList];
+                newLikedList.push(this.props.email);
+                updatedComment = {
+                    upvotes: newUpvotes,
+                    liked_by: newLikedList
+                }
+                axios.patch('http://localhost:9000/comments/' + commentID, updatedComment)
+                    .then(response => {
+                        console.log("Update Successful!");
+                        window.location.reload();
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                    })
+            } else if (likeList.includes(this.props.email)) {
+                newUpvotes = oldUpvotes - 1;
+                newLikedList = [...likeList];
+                newLikedList = newLikedList.filter(e => e !== this.props.email);
+                updatedComment = {
+                    upvotes: newUpvotes,
+                    liked_by: newLikedList
+                }
+                axios.patch('http://localhost:9000/comments/' + commentID, updatedComment)
+                    .then(response => {
+                        console.log("Update Successful!");
+                        window.location.reload();
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                    })
+            } else if (dislikeList.includes(this.props.email)) {
+                newUpvotes = oldUpvotes + 1;
+                let newDislikeList = [...dislikeList];
+                newDislikeList = newDislikeList.filter(e => e !== this.props.email);
+                newLikedList = [...likeList];
+                newLikedList.push(this.props.email);
+                updatedComment = {
+                    upvotes: newUpvotes,
+                    disliked_by: newDislikeList,
+                    liked_by: newLikedList
+                }
+                axios.patch('http://localhost:9000/comments/' + commentID, updatedComment)
+                    .then(response => {
+                        console.log("Update Successful!");
+                        window.location.reload();
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                    })
+            }
+        } else {
+            return 0;
         }
-        console.log(`new upvotes ${newUpvotes}`)
-        axios.patch('http://localhost:9000/comments/' + commentID, updatedComment)
-            .then(response => {
-                console.log("Update Successful!");
-                window.location.reload();
-            })
-            .catch(err => {
-                console.error(err.message);
-            })
     }
 
-    downvoteHandler(commentID, oldUpvotes) {
-        const newVotes = oldUpvotes - 1;
-        const updatedComment = {
-            upvotes: newVotes
+    downvoteHandler(commentID, oldUpvotes, likeList, dislikeList) {
+        if (this.props.isAuth) {
+            let newDownvotes = 0;
+            let updatedDownComment = {};
+            let newDislikeList = [];
+            //case1
+            if (!dislikeList.includes(this.props.email) && !likeList.includes(this.props.email)) {
+                newDownvotes = oldUpvotes - 1;
+                newDislikeList = [...dislikeList];
+                newDislikeList.push(this.props.email);
+                updatedDownComment = {
+                    upvotes: newDownvotes,
+                    disliked_by: newDislikeList
+                }
+                axios.patch('http://localhost:9000/comments/' + commentID, updatedDownComment)
+                    .then(response => {
+                        console.log("Update Successful!");
+                        window.location.reload();
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                    })
+            } else if (dislikeList.includes(this.props.email)) {
+                newDownvotes = oldUpvotes + 1;
+                newDislikeList = [...dislikeList];
+                newDislikeList = newDislikeList.filter(e => e !== this.props.email);
+                updatedDownComment = {
+                    upvotes: newDownvotes,
+                    disliked_by: newDislikeList
+                }
+                axios.patch('http://localhost:9000/comments/' + commentID, updatedDownComment)
+                    .then(response => {
+                        console.log("Update Successful!");
+                        window.location.reload();
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                    })
+            } else if (likeList.includes(this.props.email)) {
+                newDownvotes = oldUpvotes - 1;
+                let newLikedList = [...likeList];
+                newLikedList = newLikedList.filter(e => e !== this.props.email);
+                newDislikeList = [...dislikeList];
+                newDislikeList.push(this.props.email);
+                updatedDownComment = {
+                    upvotes: newDownvotes,
+                    disliked_by: newDislikeList,
+                    liked_by: newLikedList
+                }
+                axios.patch('http://localhost:9000/comments/' + commentID, updatedDownComment)
+                    .then(response => {
+                        console.log("Update Successful!");
+                        window.location.reload();
+                    })
+                    .catch(err => {
+                        console.error(err.message);
+                    })
+            }
+        } else {
+            return 0;
         }
-        axios.patch('http://localhost:9000/comments/' + commentID, updatedComment)
-            .then(response => {
-                console.log("Update Successful!");
-                window.location.reload();
-            })
-            .catch(err => {
-                console.error(err.message);
-            })
     }
 
     render() {
@@ -145,7 +239,7 @@ class FullPost extends Component {
                             <span>Posted by </span>
                             {/* <span>Posted by </span> */}
                             <h2 className="post-user underline" style={{ color: "steelblue" }}>{this.state.loadedPost.username}</h2>
-                            
+
                             <hr />
                             <div className="spacer"></div>
                         </div>
@@ -156,23 +250,23 @@ class FullPost extends Component {
                                     {this.state.loadedPost.description.substring(0, 3000)} ...
                                     <br></br>
                                     <br></br>
-                                    <span style={{display: 'flex', justifyContent:'center'}}><button onClick={this.toggleFullPost}>Show more</button></span>
+                                    <span style={{ display: 'flex', justifyContent: 'center' }}><button onClick={this.toggleFullPost}>Show more</button></span>
                                 </div>
-                                ) : (
-                                <div>
-                                    {this.state.loadedPost.description}
-                                    {this.state.loadedPost.description.length < 2500 ? '' : <span style={{display: 'flex', justifyContent:'center'}}><button onClick={this.toggleFullPost}>Show less</button></span>}
-                                    {this.state.loadedPost.image_src === undefined ? '' 
-                                    : this.state.loadedPost.image_src === '' ? null 
-                                    :<ModalImage
-                                    small={this.state.loadedPost.image_src}
-                                    large={this.state.loadedPost.image_src}
-                                    alt={this.state.loadedPost.title}
-                                    className="modal"
-                            />}</div>
-                            )
+                            ) : (
+                                    <div>
+                                        {this.state.loadedPost.description}
+                                        {this.state.loadedPost.description.length < 2500 ? '' : <span style={{ display: 'flex', justifyContent: 'center' }}><button onClick={this.toggleFullPost}>Show less</button></span>}
+                                        {this.state.loadedPost.image_src === undefined ? ''
+                                            : this.state.loadedPost.image_src === '' ? null
+                                                : <ModalImage
+                                                    small={this.state.loadedPost.image_src}
+                                                    large={this.state.loadedPost.image_src}
+                                                    alt={this.state.loadedPost.title}
+                                                    className="modal"
+                                                />}</div>
+                                )
                             }
-                            
+
                             {/* <img src={this.state.loadedPost.image_src}/> */}
                         </div>
 
@@ -183,31 +277,22 @@ class FullPost extends Component {
             );
             postComments = this.state.loadedcomments.map((comment, index) => (
                 <Comments
+                    key={comment._id}
                     upvotes={comment.upvotes}
                     username={comment.username}
                     body={comment.body}
                     time={comment.date}
-                    clickedUp={() => { this.upvoteHandler(comment._id, comment.upvotes) }}
-                    clickedDown={() => { this.downvoteHandler(comment._id, comment.upvotes) }} />
+                    clickedUp={() => { this.upvoteHandler(comment._id, comment.upvotes, comment.liked_by, comment.disliked_by) }}
+                    clickedDown={() => { this.downvoteHandler(comment._id, comment.upvotes, comment.liked_by, comment.disliked_by) }}
+                />
             ))
         }
         return (
             <div style={{ textAlign: "center" }}>
                 {post}
                 {postComments}
-                <Button variant="contained" color="primary" onClick={this.toggleAddComment}>Add Comment</Button>
+                <Button variant="contained" color="primary" disabled={!this.props.isAuth} onClick={this.toggleAddComment}>Add Comment</Button>
                 {this.state.displayPost ? (<div className="NewComment">
-                    <label>Username</label>
-                    <select value={this.state.username} onChange={(event) => this.setState({ username: event.target.value })}>
-                        <option value="Weevil">Weevil</option>
-                        <option value="Rex">Rex</option>
-                        <option value="Benny">Benny</option>
-                        <option value="Tyler">Tyler</option>
-                        <option value="Jasko">Jasko</option>
-                        <option value="Joey">Joey</option>
-                        <option value="Kaiba">Kaiba</option>
-                        <option value="Tristan">Tristan</option>
-                    </select>
                     <label>Body</label>
                     <textarea rows="4" value={this.state.newCommentBody} onChange={(event) => this.setState({ newCommentBody: event.target.value })} />
 
@@ -221,7 +306,9 @@ class FullPost extends Component {
 
 const mapStateToProps = state => {
     return {
-        token: state.token
+        token: state.token,
+        isAuth: state.token !== null,
+        email: state.userEmail
     }
 }
 
