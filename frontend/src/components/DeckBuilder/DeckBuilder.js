00e4a6f00a2757d8ffdef05c161955a5d2a1b756
Benny <bennyislife@gmail.com>
2020-10-06 04:38:10 -0700

    deck

--- frontend/src/components/DeckBuilder/DeckBuilder.js
+++ frontend/src/components/DeckBuilder/DeckBuilder.js
@@ -8,17 +8,15 @@ import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
 import CardData from './Cards.json';
 
-let res = [];
-let set = new Set();
 class DeckBuilder extends Component {
-
     constructor() {
         super();
         this.state = {
             loadedCards: null,
             text: '',
             cardResults: [],
-            deck: []
+            deck: [],
+            extra: []
         }
     }
 
@@ -68,31 +66,60 @@ class DeckBuilder extends Component {
     }
 
     addToDeck(text) {
-
         let arr = [];
         arr.push(text);
-        this.setState({
-            deck: [...this.state.deck, text],
-        })
+        if(this.state.deck.length <= 30) {
+            this.setState({
+                deck: [...this.state.deck, text],
+            })
+        }
+        
     }
 
-    removeCard(card) {
-        let filteredArray = this.state.deck.filter(item => item !== card)
-        this.setState({ deck: filteredArray });
+    addToExtra(text) {
+        console.log("addToExtra: " , text);
+        let arr = [];
+        arr.push(text);
+        if(this.state.extra.length <= 6) {
+            this.setState({
+                extra: [...this.state.extra, text],
+            })
+        } else{
+            return(<div>Exceeds max limit</div>) //change
+        }
+        
+    }
+
+    removeCard(card, state) {
+        let filteredArray = state.filter(item => item !== card)
+        state === this.state.deck ? this.setState({ deck: filteredArray }) :
+        this.setState({extra: filteredArray});
+    }
+
+
+    validateType(card) {
+        let types = ["Fusion Monster", "Link Monster" ,"Pendulum Effect Fusion Monster"
+        ,"Synchro Monster" ,"Synchro Pendulum Effect Monster" ,"Synchro Tuner Monster"
+        ,"XYZ Monster", "XYZ Pendulum Effect Monster"];
+        console.log(card);
+        const validate = types.some(type => card.type === type);
+        console.log(validate);
+        validate ?  this.addToExtra(card) : this.addToDeck(card);
     }
 
 
     render() {
-        let tmp = "";
+        let searchResults = "";
         let deckList = "";
+        let extraList = "";
         if (this.state.loadedCards) {
-            tmp = this.state.loadedCards[0].data.map(card => {
+            searchResults = this.state.loadedCards[0].data.map(card => {
                 return (
                     <div className="img-container">
                         <img src={card.card_images[0].image_url} alt={card.name} />
                         <a className="button"
                             style={{ cursor: "pointer" }}
-                            onClick={() => this.addToDeck(card)}>
+                            onClick={() => this.validateType(card)}>
                             Click to Add to Deck
                         </a>
                     </div>
@@ -108,7 +135,23 @@ class DeckBuilder extends Component {
                         <img src={result.card_images[0].image_url} alt={result.name} />
                         <a className="button"
                             style={{ cursor: "pointer" }}
-                            onClick={() => this.removeCard(result)}>
+                            onClick={() => this.removeCard(result, this.state.deck)}>
+                            Remove Card
+                    </a>
+
+                    </div>
+                )
+            })
+        }
+
+        if (this.state.extra) {
+            extraList = this.state.extra.map(result => {
+                return (
+                    <div className="img-container">
+                        <img src={result.card_images[0].image_url} alt={result.name} />
+                        <a className="button"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => this.removeCard(result, this.state.extra)}>
                             Remove Card
                     </a>
 
@@ -139,12 +182,14 @@ class DeckBuilder extends Component {
                 <div className="deck-wrapper">
                     <h1>Deck: </h1>
                     {deckList}
+                    <h1>Extra: </h1>
+                    {extraList}
 
                 </div>
                 <div className="cards-wrapper">
                     {/* {console.log(res)} */}
                     <div className="display-results" style={{ display: "inline-block" }}>
-                        {tmp}
+                        {searchResults}
                     </div>
 
 
