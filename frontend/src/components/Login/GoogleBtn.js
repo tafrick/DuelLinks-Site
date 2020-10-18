4bb48d3308ffad3c2495509bc15465612ddcec34
Tyler Frick <tyfrick25@gmail.com>
2020-10-17 19:02:26 -0700

    possibly fixed

--- frontend/src/components/Login/GoogleBtn.js
+++ frontend/src/components/Login/GoogleBtn.js
@@ -70,6 +70,7 @@ class GoogleBtn extends Component {
                         buttonText='Login'
                         onSuccess={this.login}
                         onFailure={this.handleLoginFailure}
+                        redirectUri="https://duellinksacademy.xyz"
                         cookiePolicy={'single_host_origin'}
                         responseType='code,token'
                     />
